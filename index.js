fde5420f68e639b9d246608bd27f54afd5ee028b
Michael Nichols <mjnichols10@yahoo.com>
2026-05-26 22:30:04 -0500

    Add interactivity and basic html

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
-function getComputerChoice() {
-  return ["rock", "paper", "scissors"][Math.floor(Math.random(3) * 3)];
-}
+const result = document.querySelector("#result");
+const humanScore = document.querySelector("#human-score");
+const computerScore = document.querySelector("#cpu-score");
 
 function getHumanChoice() {
   return prompt("What will you play? Rock, Paper, or Scissors:").toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice, score) {
+function getComputerChoice() {
+  return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+}
+
+function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    alert("It's a tie!")
+    result.textContent = "It's a tie!";
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
-    alert(`You win! ${humanChoice} beats ${computerChoice}.`);
-    score[0]++;
+    result.textContent = `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}!`;
+    humanScore.textContent++;
   } else {
-    alert(`You lose! ${humanChoice} loses to ${computerChoice}.`);
-    score[1]++;
+    result.textContent = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} loses to ${computerChoice}!`;
+    computerScore.textContent++;
   }
 }
 
-function playGame() {
-  let score = [0, 0];
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice(), score);
-  }
-  if (score[0] === score[1]) {
-    alert("The game is a tie!");
-  } else if (score[0] > score[1]) {
-    alert("You win the game!");
-  } else {
-    alert("You lose the game! The computer wins!");
+function checkWinner() {
+  if (humanScore.textContent == 5) {
+    result.textContent = "You scored 5 points! You win the game!";
+  } else if (computerScore.textContent == 5) {
+    result.textContent = "The computer scored 5 points! You lose the game!";
   }
 }
 
-alert("Welcome to the Rock Paper Scissors game!")
-playGame()
+function resetGame() {
+  result.textContent = "...";
+  humanScore.textContent = 0;
+  computerScore.textContent = 0;
+}
+
+document.body.addEventListener("click", (e) => {
+  switch (e.target.id) {
+    case "rock":
+    case "paper":
+    case "scissors":
+      playRound(e.target.id, getComputerChoice());
+      checkWinner()
+      break;
+  }
+});
